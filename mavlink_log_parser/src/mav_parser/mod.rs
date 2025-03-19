7e56dcd58837e6895e6b0f0dcac9d3483f57860a
flocked <flocked.agriculture@gmail.com>
2025-03-19 12:41:38 -0400

    Finish refactor of mav log parsers to the point of no compile errors

--- mavlink_log_parser/src/mav_parser/mod.rs
+++ mavlink_log_parser/src/mav_parser/mod.rs
@@ -3,12 +3,10 @@ mod header;
 use std::convert::TryFrom;
 use std::convert::TryInto;
 use std::fs::File;
-use std::u32;
 
-use mavlink::bytes;
-use mavlink::error::{MessageReadError, ParserError};
+use mavlink::error::MessageReadError;
 use mavlink::peek_reader::PeekReader;
-use mavlink::{MavHeader, MavlinkVersion, Message, read_versioned_msg};
+use mavlink::{MavlinkVersion, Message, read_versioned_msg};
 
 use crate::{LogEntry, MavParser};
 use header::{FileHeader, MavlinkDefinitionPayloadType};
@@ -32,19 +30,17 @@ impl TryFrom<u8> for EntryType {
     }
 }
 
-pub trait Parser {
-    fn next<M: Message>(&mut self) -> Result<LogEntry<M>, MessageReadError>;
-}
-
-struct MavlinkOnlyNoTimestampParser {
+struct MavlinkOnlyNoTimestampParser<M: Message> {
     reader: PeekReader<File>,
     mav_version: MavlinkVersion,
+    _phantom: std::marker::PhantomData<M>,
 }
 
-impl Parser for MavlinkOnlyNoTimestampParser {
-    fn next<M: Message>(&mut self) -> Result<LogEntry<M>, MessageReadError> {
+impl<M: Message> MavParser for MavlinkOnlyNoTimestampParser<M> {
+    type M = M;
+
+    fn next(&mut self) -> Result<LogEntry<M>, MessageReadError> {
         let mut entry: LogEntry<M> = LogEntry::default();
-        // TODO: think about handling error and trying next message
         let (header, message) = read_versioned_msg::<M, File>(&mut self.reader, self.mav_version)?;
         entry.mav_header = Some(header);
         entry.mav_message = Some(message);
@@ -52,29 +48,27 @@ impl Parser for MavlinkOnlyNoTimestampParser {
     }
 }
 
-struct TimestampedMavlinkOnlyParser {
+struct TimestampedMavlinkOnlyParser<M: Message> {
     reader: PeekReader<File>,
     mav_version: MavlinkVersion,
+    _phantom: std::marker::PhantomData<M>,
 }
 
-impl Parser for TimestampedMavlinkOnlyParser {
-    fn next<M: Message>(&mut self) -> Result<LogEntry<M>, MessageReadError> {
-        let mut entry: LogEntry<M> = LogEntry::default();
+impl<M: Message> MavParser for TimestampedMavlinkOnlyParser<M> {
+    type M = M;
 
+    fn next(&mut self) -> Result<LogEntry<M>, MessageReadError> {
+        let mut entry: LogEntry<M> = LogEntry::default();
         let magic_number: u8 = match self.mav_version {
             MavlinkVersion::V1 => mavlink::MAV_STX,
             MavlinkVersion::V2 => mavlink::MAV_STX_V2,
         };
         if self.reader.peek_exact(5)?[4] == magic_number {
-            // only get timestamp if it precedes a mavlink message magic number
-            // otherwise our data is misaligned
             let timestamp_raw: &[u8] = self.reader.read_exact(8)?;
             entry.timestamp = match timestamp_raw.try_into() {
                 Ok(bytes) => Some(u64::from_le_bytes(bytes)),
                 Err(_) => None,
             };
-        } else {
-            // if misaligned, ignore timestamp and let read mavlink logic jump to next magic number
         }
         let (header, message) = read_versioned_msg::<M, File>(&mut self.reader, self.mav_version)?;
         entry.mav_header = Some(header);
@@ -83,20 +77,21 @@ impl Parser for TimestampedMavlinkOnlyParser {
     }
 }
 
-pub struct MixedParser {
+pub struct MixedParser<M: Message> {
     timestamped: bool,
     reader: PeekReader<File>,
+    _phantom: std::marker::PhantomData<M>,
 }
 
-impl Parser for MixedParser {
-    fn next<M: Message>(&mut self) -> Result<LogEntry<M>, MessageReadError> {
+impl<M: Message> MavParser for MixedParser<M> {
+    type M = M;
+
+    fn next(&mut self) -> Result<LogEntry<M>, MessageReadError> {
         let mut entry: LogEntry<M> = LogEntry::default();
         let entry_type: EntryType = self
             .reader
             .read_u8()
-            .map(|value| value.try_into().unwrap_or(EntryType::Raw))?; // Default to Raw on error
-
-        // handle timestamp
+            .map(|value| value.try_into().unwrap_or(EntryType::Raw))?;
         if self.timestamped {
             let timestamp_raw: &[u8] = self.reader.read_exact(8)?;
             entry.timestamp = match timestamp_raw.try_into() {
@@ -104,20 +99,15 @@ impl Parser for MixedParser {
                 Err(_) => None,
             };
         }
-
-        // handle payload
         let payload_size: u16 = u16::from_le_bytes(
             self.reader
                 .read_exact(2)?
                 .try_into()
-                // This should never happen and is a fatal error if it does.
-                .expect("Failed to read log entry payload size. Unable to continue."),
+                .expect("Failed to read log entry payload size."),
         );
         let payload = self.reader.read_exact(payload_size as usize)?;
         match entry_type {
-            EntryType::Raw => {
-                entry.raw = Some(payload.to_vec());
-            }
+            EntryType::Raw => entry.raw = Some(payload.to_vec()),
             EntryType::Mavlink => {
                 let (header, message) =
                     read_versioned_msg::<M, File>(&mut self.reader, MavlinkVersion::V1)?;
@@ -137,37 +127,62 @@ impl Parser for MixedParser {
                 };
             }
         }
-
         Ok(entry)
     }
 }
 
-pub struct MavLogParser<P: Parser> {
-    parser: P,
+pub struct MavLogParser<M: Message + 'static> {
+    parser: Box<dyn MavParser<M = M>>,
 }
 
-impl<P: Parser> MavLogParser<P> {
+impl<M: Message + 'static> MavLogParser<M> {
     pub fn new(file_path: &str) -> Self {
         let file: File = File::open(file_path).expect("Failed to open file");
         let mut reader: PeekReader<File> = PeekReader::new(file);
 
-        // handle file header
+        let header = Self::read_file_header(&mut reader);
+
+        let mav_version = Self::determine_mavlink_version(&header);
+
+        let parser: Box<dyn MavParser<M = M>> = if header.format_flags.mavlink_only {
+            if header.format_flags.not_timestamped {
+                Box::new(MavlinkOnlyNoTimestampParser {
+                    reader,
+                    mav_version,
+                    _phantom: std::marker::PhantomData,
+                })
+            } else {
+                Box::new(TimestampedMavlinkOnlyParser {
+                    reader,
+                    mav_version,
+                    _phantom: std::marker::PhantomData,
+                })
+            }
+        } else {
+            Box::new(MixedParser {
+                timestamped: !header.format_flags.not_timestamped,
+                reader,
+                _phantom: std::marker::PhantomData,
+            })
+        };
+
+        MavLogParser { parser }
+    }
+
+    fn read_file_header(reader: &mut PeekReader<File>) -> FileHeader {
         let header_bytes: [u8; 108] = reader
             .read_exact(108)
             .expect("Failed to read file header.")
             .try_into()
             .expect("Failed to read file header.");
-        let mut header: FileHeader = FileHeader::unpack(&header_bytes);
+        let mut header = FileHeader::unpack(&header_bytes);
         let definitions_raw: &[u8] = reader
             .read_exact(header.message_definition.size as usize)
             .expect("Failed to read message definitions.");
         header.message_definition.unpack_payload(definitions_raw);
 
-        // Check supported payload types
         match header.message_definition.payload_type {
-            MavlinkDefinitionPayloadType::None => {
-                // No additional processing needed for this payload type
-            }
+            MavlinkDefinitionPayloadType::None => {}
             MavlinkDefinitionPayloadType::Utf8SpaceDelimitedUrlsForXMLFiles => {
                 panic!("Custom XML files for message definitions are not supported.");
             }
@@ -176,54 +191,27 @@ impl<P: Parser> MavLogParser<P> {
             }
         }
 
-        // check supported file format versions
         match header.format_version {
-            1 => {
-                // No additional processing needed for this format version
-            }
-            _ => {
-                panic!("Unsupported file format version.");
-            }
+            1 => {}
+            _ => panic!("Unsupported file format version."),
         }
 
-        // check supported mavlink versions
-        let mav_version = match header.message_definition.version_major {
+        header
+    }
+
+    fn determine_mavlink_version(header: &FileHeader) -> MavlinkVersion {
+        match header.message_definition.version_major {
             2 => MavlinkVersion::V2,
             1 => MavlinkVersion::V1,
-            _ => {
-                panic!("Unsupported MAVLink version.");
-            }
-        };
-
-        if header.format_flags.mavlink_only && header.format_flags.not_timestamped {
-            return MavLogParser {
-                parser: MavlinkOnlyNoTimestampParser {
-                    reader,
-                    mav_version,
-                },
-            };
-        } else if header.format_flags.mavlink_only && !header.format_flags.not_timestamped {
-            return MavLogParser {
-                parser: TimestampedMavlinkOnlyParser {
-                    reader,
-                    mav_version,
-                },
-            };
-        } else {
-            return MavLogParser {
-                parser: MixedParser {
-                    timestamped: !header.format_flags.not_timestamped,
-                    reader,
-                },
-            };
+            _ => panic!("Unsupported MAVLink version."),
         }
-
-        panic!("Unsupported file format.");
     }
 }
 
-impl<M: Message, P: Parser> MavParser<M> for MavLogParser<P> {
+impl<M: Message + 'static> MavParser for MavLogParser<M> {
+    type M = M;
+
     fn next(&mut self) -> Result<LogEntry<M>, MessageReadError> {
-        self.parser.next::<M>()
+        self.parser.next()
     }
 }
